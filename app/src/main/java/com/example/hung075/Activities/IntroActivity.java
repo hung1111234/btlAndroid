c08081d0d88146363635aa33af58060f314c3e50
hungtruong <hug3333111@gmail.com>
2024-05-08 20:36:19 +0700

    commit lan 3

--- app/src/main/java/com/example/hung075/Activities/IntroActivity.java
+++ app/src/main/java/com/example/hung075/Activities/IntroActivity.java
@@ -18,7 +18,7 @@ public class IntroActivity extends AppCompatActivity {
         binding=ActivityIntroBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.startBtn.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this, MainActivity.class)));
+        binding.startBtn.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this, LoginActivity.class)));
 
         Window w=getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
